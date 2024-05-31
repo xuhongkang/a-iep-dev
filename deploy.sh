56b8d568f45c311bac9c07ded4c24fbcccc956ed
Kane Xu <xu.hong@northeastern.edu>
2024-05-31 04:46:27 -0400

    build conf changed

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,9 @@ sudo yum install docker
 sudo service docker start 
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo yum install -y nodejs nginx python3 python3-pip 
-npm install -g pm2 yarn
 
 # Build and run the Docker containers
-sudo docker-compose build
-sudo docker-compose up -d
+sudo docker-compose up --build
 
 # Optionally, renew Let's Encrypt certificates
 # sudo apt-get install -y certbot
