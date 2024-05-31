6775d1805e857e623123df7c6077366331ed716a
Kane Xu <xu.hong@northeastern.edu>
2024-05-31 08:45:03 -0400

    changed mdb

--- app-admin/src/server.ts
+++ app-admin/src/server.ts
@@ -4,11 +4,6 @@ import payload from 'payload'
 require('dotenv').config()
 const app = express()
 
-// Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
-
 const start = async () => {
   // Initialize Payload
   await payload.init({
