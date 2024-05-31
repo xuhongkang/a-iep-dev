5569b3b3a67e7e39e47c11515cb96e893e95a1fd
Kane Xu <xu.hong@northeastern.edu>
2024-05-31 08:07:44 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # Install needed dependencies
 sudo yum update
 sudo yum install -y git docker
+sudo service docker start
 sudo git clone https://github.com/xuhongkang/aiep-app.git
-sudo service docker start 
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
