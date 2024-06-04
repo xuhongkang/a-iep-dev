1eaa176a561ca24f64c87b511acdab39c76daac2
Kane Xu <xu.hong@northeastern.edu>
2024-06-04 13:31:26 -0400

    docker

--- app-backend/app/dependencies.py
+++ app-backend/app/dependencies.py
@@ -14,7 +14,7 @@ def private_cors_dependency():
     return JSONResponse(
         {"detail": "Private CORS settings applied"},
         headers={
-            "Access-Control-Allow-Origin": "http://a-iep-dev.com",
+            "Access-Control-Allow-Origin": "http://a-iep.com",
             "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type",
         },
