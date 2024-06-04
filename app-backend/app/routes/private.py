0a9223baf5af9d58e1174ea153340e647243cd2a
Kane Xu <xu.hong@northeastern.edu>
2024-06-04 13:13:58 -0400

    docker

--- app-backend/app/routes/private.py
+++ app-backend/app/routes/private.py
@@ -1,13 +1,28 @@
 from fastapi import APIRouter, Depends
 from app.models.item import Item
 from app.dependencies import private_cors_dependency
+import requests
 
 router = APIRouter()
 
-@router.get("/", dependencies=[Depends(private_cors_dependency)])
-def read_root():
-    return {"message": "This is a private endpoint"}
+@router.post("/login/", dependencies=[Depends(private_cors_dependency)])
+def user_login(email: str, password: str):
+    url = 'http://a-iep.com/cms/api/users/login'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'email': email,
+        'password': password,
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
 
-@router.post("/items/", response_model=Item, dependencies=[Depends(private_cors_dependency)])
-def create_item(item: Item):
-    return item
+@router.post("/signup/", dependencies=[Depends(private_cors_dependency)])
+def user_signup(email: str, password: str):
+    url = 'http://a-iep.com/cms/api/users/login'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'email': email,
+        'password': password,
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
