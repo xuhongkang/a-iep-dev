570a02dd47b17447f7c23b1748fea77d58fdd3f4
Kane Xu <xu.hong@northeastern.edu>
2024-06-04 13:32:18 -0400

    docker

--- app-admin/src/collections/Users.ts
+++ app-admin/src/collections/Users.ts
@@ -4,10 +4,8 @@ const Users: CollectionConfig = {
   slug: 'users',
   auth: {
     tokenExpiration: 7200, // How many seconds to keep the user logged in
-    verify: true, // Require email verification before being allowed to authenticate
     maxLoginAttempts: 5, // Automatically lock a user out after X amount of failed logins
     lockTime: 600 * 1000, // Time period to allow the max login attempts
-
   },
   fields: [
     {
