1e4859b5a10a0d223e94b86096fffc1cd5a24c44
Kane Xu <xu.hong@northeastern.edu>
2024-06-05 18:55:10 -0400

    login

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,24 @@
 # Install needed dependencies
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the Docker repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-# Install docker sub dependencies
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install Docker sub dependencies & certbot
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin certbot
 sudo service docker start
+
+# Runs certbot to use Let's Encrypt to generate ssl certificates for the site
+sudo certbot certonly --standalone -d a-iep.org -d $DOMAIN $WWW_DOMAIN
 
 # Build and run the Docker containers
 cd ./aiep-app
