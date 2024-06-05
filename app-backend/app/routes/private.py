259f1af9c39c7d5e50f89e5b63f0e6a156475809
Kane Xu <xu.hong@northeastern.edu>
2024-06-05 12:46:27 -0400

    docker

--- app-backend/app/routes/private.py
+++ app-backend/app/routes/private.py
@@ -15,3 +15,14 @@ def user_login(email: str, password: str):
     }
     response = requests.post(url, headers=headers, json=data)
     return response.json()
+
+@router.post("/signup/", dependencies=[Depends(private_cors_dependency)])
+def user_signup(name:str, email: str, password: str):
+    url = 'http://app-admin:3000/cms/api/users'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'email': email,
+        'password': password,
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
