d36c2f1cd6f18d8fe12218f4cbf7b1094ca8e7f9
Kane Xu <xu.hong@northeastern.edu>
2024-06-05 23:11:02 -0400

    login

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # List of required environment variables
-required_env_vars=("DOMAIN" "MONGO_INITDB_ROOT_USERNAME" "MONGO_INITDB_ROOT_PASSWORD" "PAYLOAD_SECRET")
+required_env_vars="DOMAIN MONGO_INITDB_ROOT_USERNAME MONGO_INITDB_ROOT_PASSWORD PAYLOAD_SECRET"
 
 # Function to prompt for environment variables
 prompt_for_env_var() {
-  local var_name=$1
-  local var_value
+  var_name=$1
+  var_value=""
 
   # Check if the environment variable is already set
-  if [ -z "${!var_name}" ]; then
+  eval "var_value=\${$var_name}"
+  if [ -z "$var_value" ]; then
     # Prompt for the environment variable if not set
-    read -p "Enter value for $var_name: " var_value
-    export $var_name=$var_value
+    echo -n "Enter value for $var_name: "
+    read var_value
+    export $var_name="$var_value"
   else
-    echo "$var_name is already set to ${!var_name}"
+    echo "$var_name is already set to $var_value"
   fi
 }
 
@@ -38,14 +40,14 @@ sudo service docker start
 
 # Configure Environment Variables
 # Iterate through the list of required environment variables
-for var in "${required_env_vars[@]}"; do
+for var in $required_env_vars; do
   prompt_for_env_var "$var"
 done
 
 # Print the values to confirm
 echo "Final values of required environment variables:"
-for var in "${required_env_vars[@]}"; do
-  echo "$var=${!var}"
+for var in $required_env_vars; do
+  eval "echo $var=\${$var}"
 done
 
 # Runs certbot to use Let's Encrypt to generate ssl certificates for the site
