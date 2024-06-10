b97250a5312fe788f57e7629022dc6c2ddd13f0a
Kane Xu <xu.hong@northeastern.edu>
2024-06-10 00:26:55 -0400

    ssl day 2

--- app-frontend/src/api/UserAuthentication.js
+++ app-frontend/src/api/UserAuthentication.js
@@ -1,8 +1,12 @@
 export async function login() {
-  const response = await fetch('https://a-iep.org/api', {
+  const response = await fetch('https://a-iep.org/api/public/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+    },
+    body: {
+      email: 'xu.hong@ai4impact.ai',
+      password: 'Racecar48'
     }
   });
   return {isSuccessful: response.ok, message: response.json()}
