708f36150313082870e405fb81c85094eac5b253
Kane Xu <xu.hong@northeastern.edu>
2024-06-09 23:51:08 -0400

    ssl day 2

--- app-frontend/src/api/UserAuthentication.js
+++ app-frontend/src/api/UserAuthentication.js
@@ -1,9 +1,9 @@
 export async function login() {
-  const response = await fetch('https://localhost:8000/private/login');
+  const response = await fetch('https://localhost:8000/api/private/login');
   return {isSuccessful: response.ok, message: response.message}
 }
 
 export async function signup() {
-  const response = await fetch('https://localhost:8000/private/signup');
+  const response = await fetch('https://localhost:8000/api/private/signup');
   return {isSuccessful: response.ok, message: response.message}
 }
