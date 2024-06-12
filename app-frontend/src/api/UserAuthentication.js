9b840338ae918ff940c0e0a9935493cb89f74457
Kane Xu <xu.hong@northeastern.edu>
2024-06-12 13:01:21 -0400

    added protected routes

--- app-frontend/src/api/UserAuthentication.js
+++ app-frontend/src/api/UserAuthentication.js
@@ -5,35 +5,18 @@ const {setUser, clearUser } = useUserStore.getState();
 const apiBaseRoute = `https://${process.env.NEXT_PUBLIC_DOMAIN}/api`;
 
 export async function login(formData) {
-  let loginResponse = {
-    isError: false
-  };
-  const url = `${apiBaseRoute}/login`;
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(`Request failed with status ${response.status}: ${errorData.detail}`);
-    }
-    const result = await response.json();
-    if (result.errors && result.errors.length > 0) {
-        loginResponse.isError = true
-    } else if (result.token && result.user && result.user.id) {
-        setUser(result.user.id, result.token)
-        console.log('Set User', result.user.id)
-    } else {
-      throw new Error(`Unrecognized Response ${result}`)
-    }
-    return loginResponse;
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  const res = await fetch('https://a-iep.org/api/[collection-slug]/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: formData.email,
+    password: formData.password,
+  }),
+})
+  const result = await res.json()
+  console.log(result)
 }
 
 export async function signup() {
