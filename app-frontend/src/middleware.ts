f0aa25422d26a316b36e05a749230f42a3df4c5e
Kane Xu <xu.hong@northeastern.edu>
2024-06-12 10:51:55 -0400

    added protected routes

--- app-frontend/src/middleware.ts
+++ app-frontend/src/middleware.ts
@@ -1,12 +1,35 @@
-import createMiddleware from 'next-intl/middleware';
-import {pathnames, locales, localePrefix} from './config';
+import createIntlMiddleware from 'next-intl/middleware';
+import { NextResponse } from 'next/server';
 
-export default createMiddleware({
-  defaultLocale: 'en',
-  locales,
-  pathnames,
-  localePrefix
-});
+import {NextRequest} from 'next/server';
+import {pathnames, locales, localePrefix} from './config';
+import useUserStore from '@/stores/userStore';
+
+
+export default async function middleware(request: NextRequest) {
+  const { user_id, user_token } = useUserStore.getState();
+  const protectedRoutes = ['/home'];
+
+  // Check if the user is trying to access a protected route
+  for (const element of protectedRoutes) {
+    if (request.nextUrl.pathname.includes(element)) {
+    // If the user is not logged in, redirect to the login page
+      if (!user_id) {
+        return NextResponse.redirect(new URL('/', request.url));
+      }
+    }
+  }
+  const handleI18nRouting = createIntlMiddleware({
+    defaultLocale: 'en',
+    locales,
+    pathnames,
+    localePrefix
+  });
+  const response = handleI18nRouting(request);
+  return response;
+}
+
+
 
 export const config = {
   matcher: [
@@ -15,7 +38,7 @@ export const config = {
 
     // Set a cookie to remember the previous locale for
     // all requests that have a locale prefix
-    '/(en|es)/:path*',
+    '/(en|es|tl|zh)/:path*',
 
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
