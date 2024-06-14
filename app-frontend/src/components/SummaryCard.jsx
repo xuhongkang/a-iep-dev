c34607a1c2f9e42f45cd86accb9aadb7b70356af
Stephan <schmid91@mail.com>
2024-06-13 20:30:22 -0700

    more css

--- app-frontend/src/components/SummaryCard.jsx
+++ app-frontend/src/components/SummaryCard.jsx
@@ -1,15 +1,17 @@
 "use client"; // This must be the first line in the file
 import { useEffect, useState } from 'react';
 
-export default function SummaryCard({ gradient, title, subtitle, imageSrc, percentage }) {
+export default function SummaryCard({ gradient, title, subtitle, imageSrc, percentageProgress, percentageCurrent, percentageGoal }) {
   const [isCardOpen, setIsCardOpen] = useState(false);
 
   useEffect(() => {
-    setCircleCompletion(percentage);
-  }, [percentage]);
+    setCircleCompletion(percentageProgress, `progress-${title}`);
+    setCircleCompletion(percentageCurrent, `current-${title}`);
+    setCircleCompletion(percentageGoal, `goal-${title}`);
+  }, [percentageProgress, percentageCurrent, percentageGoal, title]);
 
-  function setCircleCompletion(percentage) {
-    const circle = document.getElementById(`progress-${title}`);
+  function setCircleCompletion(percentage, id) {
+    const circle = document.getElementById(id);
     if (circle) {
       const radius = 16;
       const circumference = 2 * Math.PI * radius;
@@ -40,7 +42,7 @@ export default function SummaryCard({ gradient, title, subtitle, imageSrc, perce
             zIndex: 0
           }}
         />
-        {percentage < 100 && (
+        {percentageProgress < 100 && (
           <div className="absolute right-0 sm:right-6 top-0 mt-1 sm:mt-6 flex items-center sm:scale-100 scale-75 z-20">
             <span className="mr-2 text-white hidden sm:inline">Processed</span>
             <svg width="40" height="40" viewBox="0 0 36 36">
@@ -62,52 +64,77 @@ export default function SummaryCard({ gradient, title, subtitle, imageSrc, perce
                 textAnchor="middle"
                 dominantBaseline="middle"
               >
-                {percentage}%
+                {percentageProgress}%
               </text>
             </svg>
           </div>
         )}
         <button 
           onClick={handleButtonClick} 
-          className="ml-4 p-2 rounded-full bg-white shadow z-30"
+          className="ml-4 rounded-full text-white absolute bottom-2 right-2"
+          style={{ fontSize: `30px` }}
         >
           {isCardOpen ? '-' : '+'}
         </button>
       </div>
 
       {isCardOpen && (
-        <div className="p-6 bg-white shadow-lg font-black rounded-lg mt-2">
-          {/* <h2 className="text-primary-content font-bold">{title}</h2>
-          <p className="text-primary-content mt-2">{subtitle}</p> */}
-          <p className=" mt-2 text-black-500">
-            Additional content goes here. You can add more details about the assessment or any other relevant information.
-            Additional content goes here. You can add more details about the assessment or any other relevant information.
-            Additional content goes here. You can add more details about the assessment or any other relevant information.
-            Additional content goes here. You can add more details about the assessment or any other relevant information.
+        <div className="p-6 bg-white shadow-lg font-black rounded-lg ml-2 flex flex-row justify-between">
+          <p className="mt-2 text-black-500">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
-          <span className="mr-2 text-white hidden sm:inline">Processed</span>
-            <svg width="40" height="40" viewBox="0 0 36 36">
-              <circle
-                cx="18" cy="18" r="16"
-                stroke="#d2d3d4" strokeWidth="4" fill="none"
-              />
-              <circle
-                id={`progress-${title}`}
-                cx="18" cy="18" r="16"
-                stroke="#000" strokeWidth="4" fill="none"
-                strokeDasharray="100, 100" strokeDashoffset="0"
-                transform="rotate(-90 18 18)"
-              />
-              <text
-                x="18" y="20.35"
-                fill="#000"
-                fontSize="10"
-                textAnchor="middle"
-                dominantBaseline="middle"
-              >
-                {percentage}%
-              </text>
-            </svg>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center justify-center">
+              <span className="mr-2 text-black sm:inline">Current</span>
+              <svg width="40" height="40" viewBox="0 0 36 36">
+                <circle
+                  cx="18" cy="18" r="16"
+                  stroke="#fff" strokeWidth="4" fill="none"
+                />
+                <circle
+                  id={`current-${title}`}
+                  cx="18" cy="18" r="16"
+                  stroke="#a00124" strokeWidth="4" fill="none"
+                  strokeDasharray="100, 100" strokeDashoffset="0"
+                  transform="rotate(-90 18 18)"
+                />
+                <text
+                  x="18" y="20.35"
+                  fill="#000"
+                  fontSize="10"
+                  textAnchor="middle"
+                  dominantBaseline="middle"
+                >
+                  {percentageCurrent}%
+                </text>
+              </svg>
+            </div>
+            <div className="flex items-center justify-center mt-4 self-end">
+              <span className="mr-2 text-black sm:inline">Goal</span>
+              <svg width="40" height="40" viewBox="0 0 36 36">
+                <circle
+                  cx="18" cy="18" r="16"
+                  stroke="#fff" strokeWidth="4" fill="none"
+                />
+                <circle
+                  id={`goal-${title}`}
+                  cx="18" cy="18" r="16"
+                  stroke="#00ff69" strokeWidth="4" fill="none"
+                  strokeDasharray="100, 100" strokeDashoffset="0"
+                  transform="rotate(-90 18 18)"
+                />
+                <text
+                  x="18" y="20.35"
+                  fill="#000"
+                  fontSize="10"
+                  textAnchor="middle"
+                  dominantBaseline="middle"
+                >
+                  {percentageGoal}%
+                </text>
+              </svg>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- app-frontend/src/components/SummaryCards.jsx
+++ app-frontend/src/components/SummaryCards.jsx
@@ -1,63 +1,75 @@
-import SummaryCard from '@/components/SummaryCard'
+import SummaryCard from '@/components/SummaryCard';
 
 export default function SummaryCards() {
-
   const cardData = [
     {
       gradient: "bg-gradient-to-r from-blue-900 to-blue-400",
       title: "Statewide Assessment",
       subtitle: "How your child will participate in district or state assessments.",
-      percentage: 50,
+      percentageProgress: 50,
+      percentageCurrent: 30,
+      percentageGoal: 70,
     },
     {
       gradient: "bg-gradient-to-r from-teal-800 to-teal-400",
       title: "Special Factors",
       subtitle: "What your child's unique needs are and the tools needed to help them learn better.",
-      percentage: 67,
+      percentageProgress: 67,
+      percentageCurrent: 40,
+      percentageGoal: 80,
     },
     {
       gradient: "bg-gradient-to-r from-orange-600 to-orange-300",
       title: "Annual Goals and Objectives",
       subtitle: "Where your child is expected to be in a year.",
-      percentage: 100,
+      percentageProgress: 100,
+      percentageCurrent: 90,
+      percentageGoal: 100,
     },
     {
       gradient: "bg-gradient-to-r from-purple-800 to-purple-400",
       title: "Offer of FAPE ‐ Services",
       subtitle: "Explains exactly what help/services will be available to your child.",
-      percentage: 90,
+      percentageProgress: 90,
+      percentageCurrent: 60,
+      percentageGoal: 95,
     },
     {
       gradient: "bg-gradient-to-r from-red-700 to-red-400",
       title: "Offer of FAPE ‐ Educational Setting",
       subtitle: "Where all this will happen.",
-      percentage: 60,
+      percentageProgress: 60,
+      percentageCurrent: 50,
+      percentageGoal: 85,
     },
     {
       gradient: "bg-gradient-to-r from-gray-800 to-gray-400",
       title: "Emergency Circumstances Program",
       subtitle: "How IEP will be delivered in emergencies. For instances, if school cannot operate in person.",
-      percentage: 100,
+      percentageProgress: 100,
+      percentageCurrent: 100,
+      percentageGoal: 100,
     }
   ];
 
   return (
     <div className="w-screen overflow-auto flex flex-col">
-    <div className="p-4">
-    <div className="space-y-6">
-      {cardData.map((card, index) => (
-        <SummaryCard
-          key={index}
-          gradient={card.gradient}
-          title={card.title}
-          subtitle={card.subtitle}
-          imageSrc="/images/summary_card_bg.png"
-          percentage={card.percentage}
-        />
-      ))}
-       </div>
-      {/* Add more CardComponent instances with different gradients and content as needed */}
+      <div className="p-4">
+        <div className="space-y-6">
+          {cardData.map((card, index) => (
+            <SummaryCard
+              key={index}
+              gradient={card.gradient}
+              title={card.title}
+              subtitle={card.subtitle}
+              imageSrc="/images/summary_card_bg.png"
+              percentageProgress={card.percentageProgress}
+              percentageCurrent={card.percentageCurrent}
+              percentageGoal={card.percentageGoal}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
