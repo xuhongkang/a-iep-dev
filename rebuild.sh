03778e962523e3c5f693a2f1c274142163062550
Kane Xu <xu.hong@northeastern.edu>
2024-06-14 11:44:02 -0400

    add upload

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 sudo docker-compose down
 sudo git pull
-#envsubst '${DOMAIN}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
+sudo envsubst '${DOMAIN}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
 sudo docker-compose up --build -d
