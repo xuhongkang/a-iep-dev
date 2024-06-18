c133d724393065bbc2ea8a7fcd2cff9592188246
Kane Xu <xu.hong@northeastern.edu>
2024-06-17 23:50:24 -0400

    celery pipeline

--- app-backend/app/celery_config.py
+++ app-backend/app/celery_config.py
@@ -1,12 +1,9 @@
 import os
 from celery import Celery
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
-
 app = Celery('app',  # Make sure this matches the app name
-             broker=CELERY_BROKER_URL,
-             backend=CELERY_RESULT_BACKEND)
+             broker='redis://redis:6379/0',
+             backend='redis://redis:6379/0')
 
 app.conf.update(
     task_serializer='json',
