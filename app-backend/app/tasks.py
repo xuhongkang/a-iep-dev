056fb04a77a8e6f501eabb72e3adaa93f83e6b4a
Kane Xu <xu.hong@northeastern.edu>
2024-06-17 22:19:49 -0400

    celery pipeline

--- app-backend/app/tasks.py
+++ app-backend/app/tasks.py
@@ -1,8 +1,8 @@
 from .celery_config import app
 import time
 
-@app.task
+@app.task(name='app.tasks.long_running_task')
 def long_running_task(param):
     # Simulate a long-running task
-    time.sleep(10)  # Sleep for 10 seconds
+    time.sleep(6)  # Sleep for 6 seconds
     return f'Task completed with param: {param}'
